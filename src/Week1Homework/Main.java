8540f0f04726fc32b9c97f3c9d06b542b6c0c188
Marisa Davidson <marisalynnae97@gmail.com>
2022-11-02 17:37:17 -0500

    first commit being made

---
+++ src/Week1Homework/Main.java
@@ -0,0 +1,11 @@
+package Week1Homework;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double value1 = 14.99;
+		double value2 = 15.99;
+		System.out.println(value1 + value2);
+	}
+
+}
